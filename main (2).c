b7ea4ffa824e2ab4e6c7ff56b11d5020f3759119
GamesPhilipe <54075458+GamesPhilipe@users.noreply.github.com>
2023-06-12 19:53:24 -0300

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int N, L, hash;
+    char frase[51];
+    
+    scanf("%d", &N);
+    for(int k = 0; k < N; k++){
+        hash = 0;
+        scanf("%d\n", &L);
+        
+        for(int i = 0; i < L; i++){
+            scanf("%s\n", &frase);
+            
+            for(int j = 0; j < strlen(frase); j++){
+                hash += (frase[j] - 'A') + i + j;
+            }
+        }
+        
+    printf("%d\n", hash);
+    }
+    return 0;
+}
